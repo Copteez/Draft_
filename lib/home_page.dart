a990410b33a9665dc1e6b8d9adadc7326e0728b4
Patcharapon Hathaichot <basskub555@gmail.com>
2024-08-27 04:09:47 +0700

    Add UI design
    main.dart
    
    - Loading Screen
    
    home_page.dart
    
    - AQI Dashboard UI
    - Current Stat Section
    
    Still missing
    
    - 24 hours predict UI
    - Some button function

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -36,7 +37,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   Future<Map<String, dynamic>> getForecast() async {
-    var cityName = "here";
+    var cityName = "unknow";
     var apiKey = "58619aef51181265b04347c2df10bd62a56995ef";
     var url = "api.waqi.info";
     var path = "/feed/$cityName/";
@@ -58,7 +59,30 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Weather Forecast")),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.search),
+          onPressed: () {},
+        ),
+        centerTitle: true,
+        title: Container(
+          width: 50,
+          height: 50,
+          decoration: BoxDecoration(
+            color: Color(0xFF18966C),
+            shape: BoxShape.circle,
+          ),
+          child: Center(
+            child: Icon(Icons.air, size: 30, color: Colors.white),
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.notifications),
+            onPressed: () {},
+          ),
+        ],
+      ),
       body: FutureBuilder<Map<String, dynamic>>(
         future: forecastData,
         builder: (context, snapshot) {
@@ -67,16 +91,18 @@ class _HomePageState extends State<HomePage> {
               var city = snapshot.data!['city'];
               var aqi = snapshot.data!['aqi'];
               var iaqi = snapshot.data!['iaqi'];
+              var date = DateTime.now();
+              var formattedDate =
+                  "${_getDayOfWeek(date.weekday)}, ${date.day} ${_getMonthName(date.month)} ${date.year}";
+              var formattedTime =
+                  "${date.hour % 12}:${date.minute.toString().padLeft(2, '0')} ${date.hour >= 12 ? 'PM' : 'AM'}";
+
               return ListView(
+                padding: const EdgeInsets.all(16.0),
                 children: [
-                  ListTile(
-                    title: Text("City: $city"),
-                    subtitle: Text("AQI: $aqi"),
-                  ),
-                  for (var entry in iaqi.entries)
-                    ListTile(
-                      title: Text("${entry.key.toUpperCase()}: ${entry.value['v']}"),
-                    ),
+                  _buildAQIBox(city, formattedDate, formattedTime, aqi),
+                  const SizedBox(height: 20),
+                  _buildCurrentStatBox(iaqi),
                 ],
               );
             } else if (snapshot.hasError) {
@@ -84,22 +110,375 @@ class _HomePageState extends State<HomePage> {
             }
           }
 
-          // Show a loading spinner
           return const Center(child: CircularProgressIndicator());
+        },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            label: 'Map',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.directions),
+            label: 'Path Finder',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Setting',
+          ),
+        ],
+        currentIndex: 0,
+        selectedItemColor: Color(0xFF77A1C9),
+        unselectedItemColor: Colors.grey,
+        onTap: (index) {
+          // future tab nav functions
         },
       ),
     );
   }
+
+  Widget _buildAQIBox(String city, String date, String time, int aqi) {
+    Color aqiColor;
+    String aqiLevel;
+    String aqiDescription;
+
+    // Determine AQI color, level, and description based on the value
+    if (aqi <= 50) {
+      aqiColor = Color(0xFFB5F379);
+      aqiLevel = "Good";
+      aqiDescription = "Air quality is good. It's a great day to be outside!";
+    } else if (aqi <= 100) {
+      aqiColor = Color(0xFFFFF47E);
+      aqiLevel = "Moderate";
+      aqiDescription =
+          "Air quality is acceptable. However, there may be a risk for some people, particularly those who are unusually sensitive to air pollution.";
+    } else if (aqi <= 150) {
+      aqiColor = Color(0xFFFEB14E);
+      aqiLevel = "Unhealthy for Sensitive Groups";
+      aqiDescription =
+          "Members of sensitive groups may experience health effects. The general public is less likely to be affected.";
+    } else if (aqi <= 200) {
+      aqiColor = Color(0xFFFF6274);
+      aqiLevel = "Unhealthy";
+      aqiDescription =
+          "Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects.";
+    } else if (aqi <= 300) {
+      aqiColor = Color(0xFFB46EBC);
+      aqiLevel = "Very Unhealthy";
+      aqiDescription =
+          "Health alert: everyone may experience more serious health effects.";
+    } else {
+      aqiColor = Color(0xFF975174);
+      aqiLevel = "Hazardous";
+      aqiDescription =
+          "Health warnings of emergency conditions. The entire population is more likely to be affected.";
+    }
+
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(5),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 10,
+            spreadRadius: 1,
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Icon(Icons.location_on, color: Colors.black54),
+                        SizedBox(width: 8),
+                        Expanded(
+                          child: Text(
+                            city,
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 8),
+                    Text(date, style: TextStyle(color: Colors.black54)),
+                    const SizedBox(height: 4),
+                    Text(time, style: TextStyle(color: Colors.black54)),
+                  ],
+                ),
+              ),
+              _buildAQICircle(aqi, aqiLevel, aqiColor),
+            ],
+          ),
+          const SizedBox(height: 16),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: 5,
+                height: 50,
+                color: aqiColor,
+              ),
+              SizedBox(width: 8),
+              Expanded(
+                child: Text(
+                  aqiDescription,
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 16),
+          Align(
+            alignment: Alignment.centerRight,
+            child: GestureDetector(
+              onTap: () {
+                // See more details page
+              },
+              child: Text(
+                "See more details >>",
+                style: TextStyle(
+                  color: Colors.grey,
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildAQICircle(int aqi, String aqiLevel, Color aqiColor) {
+    return Container(
+      width: 150,
+      height: 150,
+      child: CustomPaint(
+        painter: AQICirclePainter(aqi),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "AQI Value",
+                style: TextStyle(color: Colors.grey, fontSize: 14),
+              ),
+              Text(
+                "$aqi",
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 4),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 8,
+                  vertical: 2,
+                ),
+                decoration: BoxDecoration(
+                  color: aqiColor,
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                child: Text(
+                  aqiLevel,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCurrentStatBox(Map<String, dynamic> iaqi) {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(5),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 10,
+            spreadRadius: 1,
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Current stat",
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 8),
+          Divider(color: Colors.grey),
+          Column(
+            children: iaqi.entries.map((entry) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("${entry.key.toUpperCase()}",
+                        style: TextStyle(fontSize: 16, color: Colors.grey)),
+                    Text("${entry.value['v']}",
+                        style: TextStyle(fontSize: 16)),
+                  ],
+                ),
+              );
+            }).toList(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  String _getDayOfWeek(int weekday) {
+    switch (weekday) {
+      case 1:
+        return 'Monday';
+      case 2:
+        return 'Tuesday';
+      case 3:
+        return 'Wednesday';
+      case 4:
+        return 'Thursday';
+      case 5:
+        return 'Friday';
+      case 6:
+        return 'Saturday';
+      case 7:
+        return 'Sunday';
+      default:
+        return '';
+    }
+  }
+
+  String _getMonthName(int month) {
+    switch (month) {
+      case 1:
+        return 'January';
+      case 2:
+        return 'February';
+      case 3:
+        return 'March';
+      case 4:
+        return 'April';
+      case 5:
+        return 'May';
+      case 6:
+        return 'June';
+      case 7:
+        return 'July';
+      case 8:
+        return 'August';
+      case 9:
+        return 'September';
+      case 10:
+        return 'October';
+      case 11:
+        return 'November';
+      case 12:
+        return 'December';
+      default:
+        return '';
+    }
+  }
 }
 
-class ForcastType {
-  final int AQI;
-  final String City;
+class AQICirclePainter extends CustomPainter {
+  final int aqi;
 
-  ForcastType({required this.AQI, required this.City});
+  AQICirclePainter(this.aqi);
 
   @override
-  String toString() {
-    return 'ForcastType(AQI: $AQI, City: $City)';
+  void paint(Canvas canvas, Size size) {
+    final rect = Rect.fromLTRB(0, 0, size.width, size.height);
+    final startAngle = 3.14 * 0.75;
+    final sweepAngle = 3.14 * 1.5;
+    final useCenter = false;
+
+    final colors = [
+      Color(0xFFB5F379), // Green for AQI 0-50
+      Color(0xFFFFF47E), // Yellow for AQI 51-100
+      Color(0xFFFEB14E), // Orange for AQI 101-150
+      Color(0xFFFF6274), // Red for AQI 151-200
+      Color(0xFFB46EBC), // Purple for AQI 201-300
+      Color(0xFF975174), // Maroon for AQI 301-500
+    ];
+
+    final paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 15;
+
+    for (int i = 0; i < colors.length; i++) {
+      paint.color = colors[i];
+      double segmentSweepAngle = sweepAngle / colors.length;
+      canvas.drawArc(rect, startAngle + segmentSweepAngle * i, segmentSweepAngle, useCenter, paint);
+    }
+
+    int colorIndex;
+    if (aqi <= 50) {
+      colorIndex = 0;
+    } else if (aqi <= 100) {
+      colorIndex = 1;
+    } else if (aqi <= 150) {
+      colorIndex = 2;
+    } else if (aqi <= 200) {
+      colorIndex = 3;
+    } else if (aqi <= 300) {
+      colorIndex = 4;
+    } else {
+      colorIndex = 5;
+    }
+
+    double aqiStartAngle = startAngle + (sweepAngle / colors.length) * colorIndex;
+    double segmentMiddleAngle = aqiStartAngle + (sweepAngle / colors.length) / 2;
+
+    double radius = (size.width / 2) - 1;
+
+    Offset circleCenter = Offset(
+      size.width / 2 + radius * cos(segmentMiddleAngle),
+      size.height / 2 + radius * sin(segmentMiddleAngle),
+    );
+
+    final whiteCirclePaint = Paint()
+      ..color = Colors.white
+      ..style = PaintingStyle.fill;
+
+    final whiteCircleBorderPaint = Paint()
+      ..color = colors[colorIndex]
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 4;
+
+    canvas.drawCircle(circleCenter, 10, whiteCirclePaint);
+    canvas.drawCircle(circleCenter, 10, whiteCircleBorderPaint);
   }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 import 'home_page.dart';
 
 void main() {
@@ -8,7 +9,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,7 +16,117 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
       ),
-      home: HomePage(),
+      home: const LoadingScreen(),
     );
   }
+}
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),  // Animation time reduced to 2 seconds
+      vsync: this,
+    )..addListener(() {
+        setState(() {});
+      });
+    _controller.forward();
+
+    // Automatically navigate to HomePage after 2 seconds
+    Timer(const Duration(seconds: 2), () {
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => const HomePage()));
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                CustomPaint(
+                  size: const Size(200, 200),
+                  painter: CircleWavePainter(_controller.value),
+                ),
+                Icon(
+                  Icons.air,
+                  size: 60,  // Increase the size of the air symbol
+                  color: Colors.white,
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),  // Add some spacing below the circle
+            Text(
+              "Loading",
+              style: TextStyle(
+                fontSize: 24,
+                color: Color(0xFF18966C),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CircleWavePainter extends CustomPainter {
+  final double progress;
+
+  CircleWavePainter(this.progress);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = const Color(0xFF18966C)
+      ..style = PaintingStyle.fill;
+
+    final circleCenter = Offset(size.width / 2, size.height / 2);
+    final radius = size.width / 2;
+
+    // Draw white circle
+    canvas.drawCircle(circleCenter, radius, Paint()..color = Colors.white);
+
+    // Calculate wave height
+    final waveHeight = progress * size.height;
+
+    // Draw green wave filling up
+    final path = Path()
+      ..moveTo(0, size.height)
+      ..lineTo(0, size.height - waveHeight)
+      ..quadraticBezierTo(size.width / 2, size.height - waveHeight - 20,
+          size.width, size.height - waveHeight)
+      ..lineTo(size.width, size.height)
+      ..close();
+
+    canvas.clipPath(Path()..addOval(Rect.fromCircle(center: circleCenter, radius: radius)));
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
 }
